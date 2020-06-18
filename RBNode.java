ecf2672cf662b3a3cd43b65284d8ad8a0970e201
lferru <lferrufi@masonlive.gmu.edu>
2020-06-18 15:52:27 -0400

    Starting to define the tree node.

---
+++ RBNode.java
@@ -0,0 +1,19 @@
+/* Luis Ferrufino
+** 6/18/20
+** RBNode.java
+** red-black-tree-java
+ */
+
+public class RBNode<T> {
+
+    T key;
+    boolean isRed;
+
+    public RBNode(T k) {
+
+        key = k;
+        isRed = true;
+    }
+
+
+}
